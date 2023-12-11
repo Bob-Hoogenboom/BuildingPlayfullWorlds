61ac1c03f3e8057f31e8a6b3acce62fbfdf811d4
Bob Hoogenboom <bhoogenboom23@gmail.com>
2023-12-11 13:07:33 +0100

    abstract statemachine

---
+++ QuestAndDragonS/Assets/Scripts/StateMachine/BaseState.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseState<EState> where EState : Enum
+{
+    public BaseState(EState key)
+    {
+        stateKey = key;
+    }
+
+    public EState stateKey { get; private set; }
+
+    public abstract void EnterState();
+    public abstract void UpdateState();
+    public abstract void ExitState();
+    
+    public abstract EState GetNextState();
+    public abstract void OnTriggerEnter(Collider other);
+    public abstract void OnTriggerStay(Collider other);
+    public abstract void OnTriggerExit(Collider other);
+}
